c3120f8121fe75f6303184e0308c61e87cbfda7a
samuel <damosisamuel@gmail.com>
2023-01-20 11:45:53 +0100

    services file

---
+++ cobrineProject/services.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> COBSINE HOME</title>
+    <link rel="stylesheet" href="cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
+    <!-- bootsrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <!-- cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- header sTARTS HERE -->
+    <header>
+        <div class="row pt-4 ml-3 ">
+            <div class="col-2">
+                <img src="logoimgmatrix.png" alt="logo of imagecob">
+
+            </div>
+            <div class="col-6 ">
+                <div>
+                    <ul class=" myun pt-2 offset-1 ">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="services.php">SERVICES</a></li>
+                        <li>BLOG</li>
+                        <li>ABOUT</li>
+                        <li>EVENTS</li>
+                        <li>CONTACT US</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-1 pt-1">
+                <ul class="myun">
+                    <li><i style="font-size:31px;" class="fa-solid fa-magnifying-glass"></i></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- header ENDS HERE -->
+    <!-- main -->
+    <main class="">
+
+        <section class="sect5">
+
+            <div class="myjumb">
+                <div class="">
+                    <div class="col-12 ml-5">
+                        <div class="pl-4">
+                            <ul class="d-flex text-center myabt">
+                                <li> What we do </li>
+                                <hr class="ml-3 myhr pt-2">
+                            </ul>
+                            <p style="font-size: 17px;
+                            padding-left: 26px;">it is a long established fact that a reader will be distracted by the
+                                readable content of a page when</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row ml-4">
+                    <div class="col-3 ml-5">
+                        <div class="buzz">
+                            <img src="imgicon1.png" alt="">
+                            <h5 class="mt-2">Selection of Business</h5>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the form, by injected
+                                humour, or randomised</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="buzz">
+                            <img src="imgicon2.png" alt="">
+                            <h5 class="mt-2">Research and Analytics</h5>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the form, by injected
+                                humour, or randomised</p>
+
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="buzz">
+                            <img src="imgicon3.png" alt="">
+                            <h5 class="mt-2">Business Plans</h5>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the form, by injected
+                                humour, or randomised</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row ml-4 mt-4
+                ">
+                    <div class="col-3 ml-5">
+                        <div class="buzz">
+                            <img src="imgicon4.png" alt="">
+                            <h5 class="mt-2">Selection of Business</h5>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the form, by injected
+                                humour, or randomised</p>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="buzz">
+                            <img src="imgicon5.png" alt="">
+                            <h5 class="mt-2">Research and Analytics</h5>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the form, by injected
+                                humour, or randomised</p>
+
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="buzz">
+                            <img src="imgicon6.png" alt="">
+                            <h5 class="mt-2">Business Plans</h5>
+                            <p>There are many variations of passages of Lorem Ipsum available, but the form, by injected
+                                humour, or randomised</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            </div>
+
+
+        </section>
+        <!-- section 4 ends here -->
+
+    </main>
+    <!-- main -->
+    <footer class="container-fluid ">
+        <div class="row pt-4 xix">
+            <div class="col-4 ">
+
+                <div>
+                    <ul class="d-flex myf">
+                        <li><i class="fa-solid pr-2 fa-location-dot"></i></li>
+                        <li style="color: #8e8e8e;">Location</li>
+                    </ul>
+
+                    <p class="opt">About</p>
+
+                    <p class="pp">There are many variations of passages of Lorem Ipsum available, but the majority
+                        havThere are many variations of passages of Lorem Ipsum available, but the majority hav</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div>
+                    <ul class="d-flex myf">
+                        <i class="fa-solid pr-2 pt-1 fa-phone-flip"></i>
+                        <li style="color:#8e8e8e;">Call: 01 1234567</li>
+                    </ul>
+
+                    <p class="opt" style=" font-size: 25px;">Service Link </p>
+
+                    <p class="pp">There are many variations of passages of Lorem Ipsum available, but the majority
+                        havThere are many variations of passages of Lorem Ipsum available, but the majority hav</p>
+                </div>
+
+            </div>
+            <div class="col-4">
+                <div>
+                    <ul class="d-flex myf">
+                        <i class="fa-solid pr-2 pt-1 fa-envelope"></i>
+                        <li style="color: #8e8e8e;">mail@domain.com</li>
+                    </ul>
+
+                    <p class="opt" style=" font-size: 25px;">Subscribe</p>
+
+                    <div class="pt-4">
+                        <input style="background-color:black;
+                        border: none; color:white" placeholder="Enter your Email" type="text">
+                        <hr style="color: red; width: 39%;">
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+</body>
+
+</html>
